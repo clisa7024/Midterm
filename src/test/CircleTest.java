1fdb10c937c0d9eedd589c0df8dcb06d8e3a0256
lisac <lisac@LAPTOP-O0NNBQ5A.fios-router.home>
2020-03-30 23:39:22 -0400

    Miterm Project

--- src/test/CircleTest.java
+++ src/test/CircleTest.java
@@ -9,7 +9,7 @@ import midtern.Circle;
 public class CircleTest {
 
 	@Test
-	public void CircleTes() {
+	public void CircleTest() {
 		Circle c1Area = new Circle(10);
 		assertEquals(314.15, c1Area.area(), 0.01);
 	}
